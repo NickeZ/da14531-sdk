73a8c719e0196083ee5d1e34f07edd7b354e0549
Niklas Dusenlund <niklas.dusenlund@bitbox.swiss>
2024-10-03 10:26:56 +0200

    If heap runs out of space in debug mode, panic in allocator

--- src/allocator.rs
+++ src/allocator.rs
@@ -1,6 +1,6 @@
 use core::alloc::{GlobalAlloc, Layout};
 
-use crate::bindings::{ke_free, ke_malloc};
+use crate::bindings::{ke_check_malloc, ke_free, ke_malloc, KE_MEM_NON_RETENTION};
 
 pub struct Da14531Allocator;
 
@@ -8,7 +8,11 @@ impl Da14531Allocator {}
 
 unsafe impl GlobalAlloc for Da14531Allocator {
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
-        unsafe { ke_malloc(layout.size() as u32, 3) as *mut u8 }
+        #[cfg(debug_assertions)]
+        if !ke_check_malloc(layout.size() as u32, KE_MEM_NON_RETENTION as u8) {
+            panic!("Cannot allocate");
+        }
+        ke_malloc(layout.size() as u32, KE_MEM_NON_RETENTION as u8) as *mut u8
     }
 
     unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
